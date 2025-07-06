cec30df05b7d1740ed579eae8c2090ce515a6bb0
Stephen Payne <stephen@paynenz.com>
2025-07-06 21:41:46 +1200

    Fix API address and load members on demand

--- frontend/src/api/portal-api.ts
+++ frontend/src/api/portal-api.ts
@@ -3,7 +3,9 @@ import { Configuration, MembersApi } from './generated/v1'
 
 const isDevelopment = import.meta.env.MODE === 'development'
 
-const basePath = isDevelopment ? 'http://localhost:5171' : `api.${window.location.origin}`
+const basePath = isDevelopment
+  ? 'http://localhost:5171'
+  : `https://api.portal.canterburyunderwater.org.nz`
 
 const buildApiConfig = async () => {
   return new Configuration({})
--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -4,14 +4,16 @@ import { buildMembersApi } from '@/api/portal-api'
 
 const members = ref<Member[]>([])
 
-onMounted(async () => {
+async function loadMembers() {
   const api = await buildMembersApi()
 
   members.value = (await api.v1MembersGet()).data.members
-})
+}
 </script>
 
 <template>
+  <VBtn @click="loadMembers">Load Members</VBtn>
+
   <VRow class="match-height">
     <VCol v-for="member in members" :key="member.id">
       {{ member.firstName }}
