66b87499b3307a5080f19691b4196e0dd29c1635
Stephen Payne <stephen@paynenz.com>
2025-08-25 22:03:51 +1200

    Include IsCurrent when getting individual rate plans

--- backend/PortalApi/Features/Bookings/Admin/BookingRatePlans/Get/Handler.cs
+++ backend/PortalApi/Features/Bookings/Admin/BookingRatePlans/Get/Handler.cs
@@ -3,11 +3,12 @@ using CanterburyUnderwater.PortalApi.DataAccess;
 using CanterburyUnderwater.PortalApi.EndpointHandling;
 using CanterburyUnderwater.PortalApi.ErrorHandling;
 using CanterburyUnderwater.PortalApi.Features.Bookings.Models;
+using CanterburyUnderwater.PortalApi.Services;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace CanterburyUnderwater.PortalApi.Features.Bookings.Admin.BookingRatePlans.Get;
 
-public class Handler(PortalDbContext db, IMapper mapper)
+public class Handler(PortalDbContext db, IBookingService bookingService, IMapper mapper)
     : IRequestEndpointHandler<Contracts.Request, Results<ProblemHttpResult, Ok<Contracts.Response>>>
 {
     public async Task<Results<ProblemHttpResult, Ok<Contracts.Response>>> HandleAsync(Contracts.Request request,
@@ -17,9 +18,21 @@ public class Handler(PortalDbContext db, IMapper mapper)
 
         if (ratePlan == null) return ProblemTypedResults.NotFound<BookingRatePlanModel>(request.RatePlanId);
 
+        var model = mapper.Map<BookingRatePlanModel>(ratePlan);
+
+        try
+        {
+            var current = await bookingService.GetCurrentRatePlanAsync(ct);
+            model.IsCurrent = model.Id == current.Id;
+        }
+        catch
+        {
+            // Ignore if there is no current rate plan
+        }
+
         var response = new Contracts.Response
         {
-            RatePlan = mapper.Map<BookingRatePlanModel>(ratePlan)
+            RatePlan = model
         };
 
         return TypedResults.Ok(response);
